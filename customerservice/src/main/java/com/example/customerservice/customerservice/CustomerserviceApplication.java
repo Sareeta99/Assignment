32b183a7beb673bfcf455b19870a8b4f4694eedc
Sareeta99 <100314354+Sareeta99@users.noreply.github.com>
2022-03-04 15:47:55 +0530

    Add files via upload

--- customerservice/src/main/java/com/example/customerservice/customerservice/CustomerserviceApplication.java
+++ customerservice/src/main/java/com/example/customerservice/customerservice/CustomerserviceApplication.java
@@ -1,8 +1,14 @@
 package com.example.customerservice.customerservice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @SpringBootApplication
 @ComponentScan("com.example.customerservice.customerservice")
@@ -11,5 +17,11 @@ public class CustomerserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerserviceApplication.class, args);
 	}
+	
+@Bean
+@LoadBalanced
+public RestTemplate restTemplate() {
+	return new RestTemplate();
+}
 
 }
